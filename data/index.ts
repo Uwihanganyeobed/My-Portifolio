8000b350f2b07fffb3a5cb8121aff1ee8c4bc280
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-07-13 17:14:48 +0200

    set

--- data/index.ts
+++ data/index.ts
@@ -182,7 +182,7 @@ export const workExperience = [
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
+    title: "Mobile App Dev - Obed Tech",
     desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp2.svg",
